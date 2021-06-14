5cef175e7855f995d4f3f68498f4f263d9a74264
Dexusan <24143107+Dexusan@users.noreply.github.com>
2021-06-14 00:34:27 -0500

    Add Harmony Manager

---
+++ Tweaker/src/Util/HarmonyManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using HarmonyLib;
+
+namespace Dex.Tweaker.Util
+{
+    class HarmonyManager
+    {
+        public HarmonyManager(string id)
+        {
+            ID = id;
+            Instance = new Harmony(id);
+        }
+        public void Patch(Type original, string method, Type patch, bool prefix = false, bool postfix = false)
+        {
+            var harmonyPrefix = prefix ? new HarmonyMethod(patch, "Prefix") : null;
+            var harmonyPostfix = postfix ? new HarmonyMethod(patch, "Postfix") : null;
+            this.Instance.Patch(original.GetMethod(method), harmonyPrefix, harmonyPostfix);
+        }
+        public void Unpatch(Type original, string method, int type = 1)
+        {
+            this.Instance.Unpatch(original.GetMethod(method), (HarmonyPatchType)type, ID);
+        }
+        public void Unpatch(Type original, string method, HarmonyPatchType type = HarmonyPatchType.Prefix)
+        {
+            this.Instance.Unpatch(original.GetMethod(method), type, ID);
+        }
+        public string ID { get; set; }
+        public Harmony Instance { get; set; }
+    }
+}
