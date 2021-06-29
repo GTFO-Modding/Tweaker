9c63aa25e340abcafd24faf16387517caa854a22
Dexusan <24143107+Dexusan@users.noreply.github.com>
2021-06-29 04:08:15 -0500

    Refactor biotracker setup

--- Tweaker/src/Patch/EnemyScanner_Setup.cs
+++ Tweaker/src/Patch/EnemyScanner_Setup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using Dex.Tweaker.Core;
 using Dex.Tweaker.Util;
 using HarmonyLib;
@@ -12,40 +13,47 @@ namespace Dex.Tweaker.Patch
     {
         public static void Prefix(ref ItemDataBlock data, EnemyScanner __instance)
         {
-            Log.Debug(
-                "EnemyScanner Setup" +
-                $"\n\tscanConeDotMin:{__instance.m_scanConeDotMin}" +
-                $"\n\tmaxScanWorldRadius:{__instance.m_maxScanWorldRadius}" +
-                $"\n\tlocalObjRadius:{__instance.m_localObjRadius}" +
-                $"\n\tenemyObjMinScale:{__instance.m_enemyObjMinScale}" +
-                $"\n\tmaxObjs:{ __instance.m_maxObjs}" +
-                $"\n\tmaxCourseNodeDistance:{__instance.m_maxCourseNodeDistance}" +
-                $"\n\tscanDelay:{__instance.m_scanDelay}" +
-                $"\n\tpulseDuration:{__instance.m_pulseDuration}" +
-                $"\n\tposDelay:{__instance.m_posDelay}" +
-                $"\n\ttagDuration:{__instance.m_tagDuration}" +
-                $"\n\trechargeDuration:{__instance.m_rechargeDuration}"
-            );
+            object[] information =
+            {
+                __instance.m_scanConeDotMin,
+                __instance.m_maxScanWorldRadius,
+                __instance.m_localObjRadius,
+                __instance.m_enemyObjMinScale,
+                __instance.m_maxObjs,
+                __instance.m_maxCourseNodeDistance,
+                __instance.m_scanDelay,
+                __instance.m_pulseDuration,
+                __instance.m_posDelay,
+                __instance.m_tagDuration,
+                __instance.m_rechargeDuration
+            };
 
+            var logOutput = new StringBuilder();
+            var format = " {0,21}: {1}";
+
+            logOutput.AppendLine("Enemyscanner Setup");
+            foreach(var info in information) logOutput.AppendLine(string.Format(format, nameof(info), info));
             foreach (var config in ConfigManager.BioTracker.Config)
             {
                 if (!config.internalEnabled
                     || config.ItemID != data.persistentID)
                     continue;
-                __instance.m_scanConeDotMin = config.scanConeDotMin;
-                __instance.m_maxScanWorldRadius = config.maxScanWorldRadius;
-                __instance.m_localObjRadius = config.localObjRadius;
-                __instance.m_enemyObjMinScale = config.enemyObjMinScale;
-                __instance.m_maxObjs = config.maxObjs;
-                __instance.m_maxCourseNodeDistance = config.maxCourseNodeDistance;
-                __instance.m_scanDelay = config.scanDelay;
-                __instance.m_pulseDuration = config.pulseDuration;
-                __instance.m_posDelay = config.posDelay;
-                __instance.m_tagDuration = config.tagDuration;
-                __instance.m_rechargeDuration = config.rechargeDuration;
+                information[ 0] = config.scanConeDotMin;
+                information[ 1] = config.maxScanWorldRadius;
+                information[ 2] = config.localObjRadius;
+                information[ 3] = config.enemyObjMinScale;
+                information[ 4] = config.maxObjs;
+                information[ 5] = config.maxCourseNodeDistance;
+                information[ 6] = config.scanDelay;
+                information[ 7] = config.pulseDuration;
+                information[ 8] = config.posDelay;
+                information[ 9] = config.tagDuration;
+                information[10] = config.rechargeDuration;
+                logOutput.AppendLine($"Loaded {config.name}[{config.ItemID}]");
                 break;
             }
             //m_showingNoTargetsTimer is set in UpdateTagProgress
+            Log.Debug(logOutput.ToString());
         }
     }
 }
