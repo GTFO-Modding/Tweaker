a2d71adc4af2e56877935930f55423d58a0ea1e4
Dexusan <24143107+Dexusan@users.noreply.github.com>
2021-06-29 03:15:49 -0500

    Update default values

--- Tweaker/src/DataTransfer/RundownLayout.cs
+++ Tweaker/src/DataTransfer/RundownLayout.cs
@@ -6,25 +6,32 @@ namespace Dex.Tweaker.DataTransfer
     {
         public ExpeditionButton[] Tier1 { get; set; } = new ExpeditionButton[]
         {
-            new ExpeditionButton("A1", -375f, 0f, 0f, 2f),
-            new ExpeditionButton("A2",  375f, 0f, 0f, 2f)
+            new ExpeditionButton("A1", 1.666667f, -450f,    0, 0),
+            new ExpeditionButton("A2", 1.666667f,     0, -270, 0),
+            new ExpeditionButton("A3", 1.666667f,  450f,    0, 0)
         };
         public ExpeditionButton[] Tier2 { get; set; } = new ExpeditionButton[]
         {
-            new ExpeditionButton("B1", -525f,    0f, 0f, 1.428571f),
-            new ExpeditionButton("B2",    0f, -315f, 0f, 1.428571f),
-            new ExpeditionButton("B3",  525f,    0f, 0f, 1.428571f)
+            new ExpeditionButton("B1", 1.428571f, -525,    0, 0),
+            new ExpeditionButton("B2", 1.428571f, -175, -210, 0),
+            new ExpeditionButton("B3", 1.428571f,  175, -210, 0),
+            new ExpeditionButton("B4", 1.428571f,  525,    0, 0)
         };
         public ExpeditionButton[] Tier3 { get; set; } = new ExpeditionButton[]
         {
-            new ExpeditionButton("C1", -412.5f, 0f, 0f, 1.818182f),
-            new ExpeditionButton("C2",  412.5f, 0f, 0f, 1.818182f)
+            new ExpeditionButton("C1", 1.818182f, -412.5f,       0, 0),
+            new ExpeditionButton("C2", 1.818182f,       0, -247.5f, 0),
+            new ExpeditionButton("C3", 1.818182f,  412.5f,       0, 0)
         };
         public ExpeditionButton[] Tier4 { get; set; } = new ExpeditionButton[]
         {
-            new ExpeditionButton("D1", -75f, 0f, 0f, 10f)
+            new ExpeditionButton("D1", 2.5f, -300, 0, 0),
+            new ExpeditionButton("D2", 2.5f,  300, 0, 0)
         };
-        public ExpeditionButton[] Tier5 { get; set; }
+        public ExpeditionButton[] Tier5 { get; set; } = new ExpeditionButton[]
+        {
+            new ExpeditionButton("E1", 10f, -75, 0, 0)
+        };
         public bool HideProgression { get; set; } = true;
         public bool HideTiers { get; set; } = true;
         public bool internalEnabled { get; set; } = false;
