c145c45342875c89f05be10a235d3b1243a9454d
Dexusan <24143107+Dexusan@users.noreply.github.com>
2021-06-29 04:07:09 -0500

    Change log format

--- Tweaker/src/Patch/Dam_SyncedDamageBase_RegisterDamage.cs
+++ Tweaker/src/Patch/Dam_SyncedDamageBase_RegisterDamage.cs
@@ -7,13 +7,7 @@ namespace Dex.Tweaker.Patch
     [HarmonyPatch(typeof(Dam_SyncedDamageBase), "RegisterDamage")]
     class Dam_SyncedDamageBase_RegisterDamage
     {
-        public static void Prefix(ref float dam, Dam_SyncedDamageBase __instance)
-        {
-            HealthPre = __instance.Health;
-            HealthPost = HealthPre - dam;
-            Log.Debug($"Register Damage\n\tbase target: {__instance.name}\n\tbase health: {HealthPre}/{__instance.HealthMax}\n\tamount: {dam}\n\thealth remainder: {HealthPost}");
-        }
-        static float HealthPre { get; set; }
-        static float HealthPost { get; set; }
+        public static void Postfix(ref float dam, Dam_SyncedDamageBase __instance)
+            => Log.Debug($"Registered Damage: {dam} Health: {__instance.Health} / {__instance.HealthMax} Target: {__instance.name}");
     }
 }
